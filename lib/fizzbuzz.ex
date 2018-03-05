74b4bf37d1bced74a2e0c7cd5e9cf8b3a59dbc2d
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-05 14:14:01 +0000

    Add returning normal number to fizzbuzz

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -5,6 +5,7 @@ defmodule Fizzbuzz do
       rem(number, 15) === 0 -> "Fizzbuzz"
       rem(number, 3) === 0 -> "Fizz"
       rem(number, 5) === 0 -> "Buzz"
+      true -> number
     end
   end
 
--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -3,14 +3,18 @@ defmodule FizzbuzzTest do
   doctest Fizzbuzz
 
   test "FizzBuzz returns fizz when passed 3" do
-    assert Fizzbuzz.fizzbuzz(3) == "Fizz"
+    assert Fizzbuzz.fizzbuzz(3) === "Fizz"
   end
 
   test "Fizzbuzz returns buzz when passed 5" do
-    assert Fizzbuzz.fizzbuzz(5) == "Buzz"
+    assert Fizzbuzz.fizzbuzz(5) === "Buzz"
   end
 
   test "Fizzbuzz returns fizzbuzz when passed 15" do
-    assert Fizzbuzz.fizzbuzz(15) == "Fizzbuzz"
+    assert Fizzbuzz.fizzbuzz(15) === "Fizzbuzz"
+  end
+
+  test "Fizzbuzz returns number" do
+    assert Fizzbuzz.fizzbuzz(1) === 1
   end
 end
